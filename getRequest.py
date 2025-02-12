2b595e874ebf8806c0dc3b8077f99099f1152095
Manoj Potru <manoj.potru@healthcatalyst.com>
2025-02-12 20:00:24 +0530

    added files

--- getRequest.py
+++ getRequest.py
@@ -9,7 +9,7 @@ region = "us-east-1"
 datastore_id = ""
 
 # HealthLake endpoint for Patient creation
-url = f"https://healthlake.{region}.amazonaws.com/datastore/{datastore_id}/r4/Patient"
+url = f"https://healthlake.{region}.amazonaws.com/datastore/{datastore_id}/r4/Patient/"
 
 # AWS4Auth for signing requests
 auth = AWS4Auth(
@@ -25,39 +25,18 @@ headers = {
     "Accept": "application/json",
 }
 
-# Patient resource payload
-patient_data = {
-    "resourceType": "Patient",
-    "identifier": [
-        {
-            "system": "urn:oid:1.2.36.146.595.217.0.1",
-            "value": "12345"
-        }
-    ],
-    "name": [
-        {
-            "family": "Test",
-            "given": [
-                "AWS",
-                "Healthlake"
-            ]
-        }
-    ],
-    "gender": "male",
-    "birthDate": "1992-02-10"
-}
+
 
 # Make the POST request
-response = requests.post(
+response = requests.get(
     url,
     auth=auth,
-    headers=headers,
-    data=json.dumps(patient_data)
+    headers=headers
 )
 
 # Output the response
 print(f"Status Code: {response.status_code}")
 try:
-    print(f"Response: {json.dumps(response.json(), indent=4)}")
+    print(f"Response: {response.json()}")
 except json.JSONDecodeError:
     print("Response:", response.text)
--- search request.py
+++ search request.py
@@ -9,7 +9,8 @@ region = "us-east-1"
 datastore_id = ""
 
 # HealthLake endpoint for Patient creation
-url = f"https://healthlake.{region}.amazonaws.com/datastore/{datastore_id}/r4/Patient"
+url = f"https://healthlake.{region}.amazonaws.com/datastore/{datastore_id}/r4/Patient?name=Test"
+
 
 # AWS4Auth for signing requests
 auth = AWS4Auth(
@@ -25,34 +26,12 @@ headers = {
     "Accept": "application/json",
 }
 
-# Patient resource payload
-patient_data = {
-    "resourceType": "Patient",
-    "identifier": [
-        {
-            "system": "urn:oid:1.2.36.146.595.217.0.1",
-            "value": "12345"
-        }
-    ],
-    "name": [
-        {
-            "family": "Test",
-            "given": [
-                "AWS",
-                "Healthlake"
-            ]
-        }
-    ],
-    "gender": "male",
-    "birthDate": "1992-02-10"
-}
 
 # Make the POST request
-response = requests.post(
+response = requests.get(
     url,
     auth=auth,
-    headers=headers,
-    data=json.dumps(patient_data)
+    headers=headers
 )
 
 # Output the response
